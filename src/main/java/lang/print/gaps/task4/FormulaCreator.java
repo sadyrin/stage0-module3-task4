b9733755aa3f90882ef497e2d916150b224b1b15
sadyrin <sadyrin@gmail.com>
2023-01-25 20:21:53 +0200

    Done task "Module #3. Task #4. Primitives"

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,15 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        byte k1 = 9;
+        byte k2 = 5;
+        byte k3 = 2;
+        byte k4 = 7;
+        byte k5 = 4;
+
+        char a = 'a';
+        char b = 'b';
+
+        String formula = "(9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2)";
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = 99;
+        int h = 104;
+        int a = 97;
+        int r = 114;
+
+        System.out.println(c + h + a + r);
     }
 }
